77a7379db2a8a004f8b59d62e7e7481a7f9c3ebe
Tricia <tverbanic@gmail.com>
2021-02-11 17:13:19 +0000

    Day4 cleanup

---
+++ mix-list/mix_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+my_list = ["182.168.0.5", 5060, "UP"]
+
+print("The first item in the list (IP): " + my_list[0])
+
+print("The second item in the list (port): " + str(my_list[1]) )
+
+print("The last item in the list (state): " + my_list[2] )
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
